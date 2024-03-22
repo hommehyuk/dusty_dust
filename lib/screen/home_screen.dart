9302ef3ce323d1da7a4851490e95794416b43f28
hommehyuk <bin6827@gmail.com>
2024-03-22 14:41:44 +0900

    HourlyCard 작업

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,13 @@
+import 'package:dusty_dust/component/card_title.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
@@ -23,6 +27,58 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CategoryCard(),
+                SizedBox(height: 16.0),
+                MainCard(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CardTitle(
+                        title: '시간별 미세먼지',
+                      ),
+                      Column(
+                        children: List.generate(
+                          24,
+                          (index) {
+                            final now = DateTime.now();
+                            final hour = now.hour;
+                            int currentHour = hour - index;
+
+                            // 3 2 1 0 23 22 21
+                            if (currentHour - index < 0) {
+                              currentHour += 24;
+                            }
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                                vertical: 4.0,
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '$currentHour',
+                                    ),
+                                  ),
+                                  Image.asset(
+                                    'asset/img/good.png',
+                                    height: 20.0,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '좋음',
+                                      textAlign: TextAlign.right,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
