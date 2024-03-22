f10e2cf4716ebd866ce3335061d098f8c78fd8e2
hommehyuk <bin6827@gmail.com>
2024-03-22 14:02:19 +0900

    Drawer 디자인

--- lib/component/main_app_bar.dart
+++ lib/component/main_app_bar.dart
@@ -16,43 +16,46 @@ class MainAppBar extends StatelessWidget {
       expandedHeight: 500,
       flexibleSpace: FlexibleSpaceBar(
         background: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                '서울',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+          child: Container(
+            margin: EdgeInsets.only(top: kToolbarHeight),
+            child: Column(
+              children: [
+                Text(
+                  '서울',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              Text(
-                DateTime.now().toString(),
-                style: ts.copyWith(
-                  fontSize: 20.0,
+                Text(
+                  DateTime.now().toString(),
+                  style: ts.copyWith(
+                    fontSize: 20.0,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Image.asset(
-                'asset/img/mediocre.png',
-                width: MediaQuery.of(context).size.width / 2,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                '보통',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+                SizedBox(height: 20.0),
+                Image.asset(
+                  'asset/img/mediocre.png',
+                  width: MediaQuery.of(context).size.width / 2,
                 ),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '나쁘지 않네요!',
-                style: ts.copyWith(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
+                SizedBox(height: 20.0),
+                Text(
+                  '보통',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 8.0),
+                Text(
+                  '나쁘지 않네요!',
+                  style: ts.copyWith(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF6495ED);
+
+const darkColor = Color(0xFF011599);
+
+const lightColor = Color(0xFF87CEEB);
