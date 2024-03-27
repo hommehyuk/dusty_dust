4db8e2edca78c30a57ebee32e9946821aebb279b
hommehyuk <bin6827@gmail.com>
2024-03-27 17:10:32 +0900

    Adapter 생성 및 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
+  for (ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
